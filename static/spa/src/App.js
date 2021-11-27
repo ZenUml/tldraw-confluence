9879b961c050fec3e087407dddb4d644ccad678a
MrCoder <eagle.xiao@gmail.com>
2021-11-27 19:10:00 +1100

    Removed a lot of unused code.

--- static/spa/src/App.js
+++ static/spa/src/App.js
@@ -1,78 +1,23 @@
-import React, { useEffect, useState, Fragment } from 'react';
+import React, { useEffect, useState} from 'react';
 import { invoke } from '@forge/bridge';
 import {Tldraw} from '@tldraw/tldraw';
 
 // Atlaskit
-import { LoadingButton as Button } from '@atlaskit/button';
-import { Checkbox } from '@atlaskit/checkbox';
-import CloseIcon from '@atlaskit/icon/glyph/editor/close';
-import TrashIcon from '@atlaskit/icon/glyph/editor/remove';
-import Textfield from '@atlaskit/textfield';
-import Lozenge from '@atlaskit/lozenge';
 import Spinner from '@atlaskit/spinner';
 
 // Custom Styles
 import {
-  Card, Row, Icon, IconContainer, Status, SummaryActions, SummaryCount, SummaryFooter,
-  ScrollContainer, Form, LoadingContainer
+  Card, ScrollContainer, LoadingContainer
 } from './Styles';
 
 function App() {
   const [todos, setTodos] = useState(null);
-  const [input, setInput] = useState('');
   const [isFetched, setIsFetched] = useState(false);
-  const [isDeleteAllShowing, setDeleteAllShowing] = useState(false);
-  const [isDeletingAll, setDeletingAll] = useState(false);
 
   if (!isFetched) {
     setIsFetched(true);
     invoke('get-all').then(setTodos);
   }
-
-  const createTodo = async (label) => {
-    const newTodoList = [...todos, { label, isChecked: false, isSaving: true }];
-
-    setTodos(newTodoList);
-  }
-
-  const toggleTodo = (id) => {
-    setTodos(
-      todos.map(todo => {
-        if (todo.id === id) {
-          return { ...todo, isChecked: !todo.isChecked, isSaving: true };
-        }
-        return todo;
-      })
-    )
-  }
-
-  const deleteTodo = (id) => {
-    setTodos(
-      todos.map(todo => {
-        if (todo.id === id) {
-          return { ...todo, isDeleting: true };
-        }
-        return todo;
-      })
-    )
-  }
-
-  const deleteAllTodos = async () => {
-    setDeletingAll(true);
-
-    await invoke('delete-all');
-
-    setTodos([]);
-    setDeleteAllShowing(false);
-    setDeletingAll(false);
-  }
-
-  const onSubmit = (e) => {
-    e.preventDefault();
-    createTodo(input);
-    setInput('');
-  };
-
   useEffect(() => {
     if (!todos) return;
     if (!todos.find(todo => todo.isSaving || todo.isDeleting)) return;
@@ -104,39 +49,6 @@ function App() {
       </Card>
     );
   }
-
-  const completedCount = todos.filter(todo => todo.isChecked).length;
-  const totalCount = todos.length;
-
-  const Rows = () => (
-    <Fragment>
-      TLDrawer
-      <Tldraw />
-    </Fragment>
-  );
-
-  const DeleteAll = () => isDeleteAllShowing ? (
-    <Button
-      appearance="danger"
-      spacing="compact"
-      isLoading={isDeletingAll}
-      isDisabled={isDeletingAll}
-      onClick={deleteAllTodos}
-    >
-      Delete All
-    </Button>
-  ) : (
-    <Button appearance="subtle" spacing="none" onClick={() => setDeleteAllShowing(true)}>
-      <IconContainer>
-        <Icon>
-          <TrashIcon />
-        </Icon>
-      </IconContainer>
-    </Button>
-  );
-
-  const CompletedLozenge = () => <Lozenge>{completedCount}/{totalCount} Completed</Lozenge>;
-
   return (
     <Card>
       <ScrollContainer>
