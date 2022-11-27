925dc3f8332c1ca6a77d0ce624c30633dd955f06
MrCoder <eagle.xiao@gmail.com>
2022-11-27 12:07:04 +1100

    Can resize the canvas.
    Not persisting the height.

--- static/spa/src/AppFactory.js
+++ static/spa/src/AppFactory.js
@@ -1,26 +1,36 @@
 import React from "react";
 import {defaultDocument} from "./defaultDocument";
-import {Card} from "./Styles";
 import {Tldraw} from "@tldraw/tldraw";
+import {Rnd} from "react-rnd";
 
-export default function(invoke) {
+const style = {
+  display: "flex",
+  border: "solid 1px #ddd",
+  background: "#f0f0f0",
+  height: '400px',
+  position: 'relative',
+  marginBottom: '10px',
+};
+
+export default function (invoke) {
   return function App() {
     const [isFetched, setIsFetched] = React.useState(false);
     const [height, setHeight] = React.useState(400);
     const rInitialDocument = React.useRef(
         defaultDocument
     )
-    if(!isFetched) {
+    if (!isFetched) {
       invoke('get-all').then((doc) => {
         console.debug('[App] get-all', doc, doc.id);
 
-        if(doc && doc.id) {
+        if (doc && doc.id) {
           console.debug('[App] get-all', doc);
           rInitialDocument.current = doc;
           setIsFetched(true);
         }
       });
     }
+
     function onPersist(app) {
       if (JSON.stringify(app.document) === JSON.stringify(rInitialDocument.current)) {
         console.debug('[App] onPersist skipped');
@@ -30,15 +40,25 @@ export default function(invoke) {
       invoke('update', app.document);
     }
     return (
-        <Card style={{height: `${height}px`}} className='group'>
-          <div className='hidden group-hover:block'>
-            <button onClick={() => setHeight(Math.min(height + 100, 2000))}>Increase height</button>
-            <button onClick={() => setHeight(Math.max(height - 100, 200))}>Decrease height</button>
-          </div>
-          <div className='mt-5'>
-            <Tldraw onPersist={onPersist} document={rInitialDocument.current} />
-          </div>
-        </Card>
+        <div>
+          <Rnd
+              enableResizing={{bottom: true, right: true}}
+              disableDragging={true}
+              style={style}
+              size={{height: `${height}px`, width: '100%'}}
+
+              onResizeStop={(e, direction, ref, delta, position) => {
+                console.log('ref.style.height', ref.style.height);
+                const h = Number(ref.style.height.slice(0, -2));
+                console.log('setting height', h);
+                setHeight(h);
+              }}
+          >
+            <div>
+              <Tldraw onPersist={onPersist} document={rInitialDocument.current}/>
+            </div>
+          </Rnd>
+        </div>
     );
   }
 
